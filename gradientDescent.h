c4a50f449fb62247594c26b74e9664c6a3cd89d6
Sruthik P <psruthik@gmail.com>
2017-12-18 08:34:13 +0000

    ++

---
+++ gradientDescent.h
@@ -0,0 +1,30 @@
+#ifndef GRADIENTDESCENT_H
+#define GRADIENTDESCENT_H
+
+#include <stdio.h>
+#include "matrix.h"
+//Include the header file that contains the function to be used
+matrix numericalGradientDescent(float (*lossFunction)(matrix, matrix), matrix Xtr, matrix Ytr, matrix weights, float h=0.0001)
+{
+    float loss = lossFunction(Xtr, Ytr);
+    float temp;
+    float fxh;
+    matrix gradient = create(weights.rows, weights.cols);
+
+    for(int i = 0; i < weights.rows; i++)
+        for(int j = 0; j < weights.cols; j++)
+        {
+            temp = weights.vals[i][j];
+            weights.vals[i][j] += h;
+            fxh = lossFunction(Xtr, Ytr);
+            weights.vals[i][j] = temp;
+            gradient.vals[i][j] = (fxh - loss)*(1/h);
+        }
+    return gradient;
+}
+
+matrix analyticalGradientDescent(float (*lossFunction)(matrix, matrix), matrix Xtr, matrix Ytr)
+{
+    
+}
+#endif
---
+++ softmax.h
@@ -0,0 +1,32 @@
+#ifndef SOFTMAX_H
+#define SOFTMAX_H
+
+#include "matrix.h"
+#include <time.h>
+
+matrix weights;
+matrix loss;
+matrix hypothesis;
+
+void softmax_init(matrix Xtr, matrix Ytr)
+{
+    weights = create(10, 3073);
+    loss = create(50000, 1);
+    hypothesis = create(50000, 10);
+
+    for(int i = 0 ; i < weights.rows; i++)
+        weights.vals[i][(weights.cols) - 1] = 1.0;
+
+    srand(time(NULL));
+
+    for(int i = 0; i < weights.rows; i++)
+        for(int j = 0; j < weights.cols - 1; j++)
+                weights.vals[i][j] = 0.01 * (rand() % 2);
+                
+    Xtr = reshape(Xtr, Xtr.rows, Xtr.cols+1);
+
+    for(int i = 0; i < Xtr.rows; i++)
+            Xtr.vals[i][Xtr.cols - 1] = 1.0;
+}
+
+#endif
